e57d4e384dea11ddbbac102ef70db02ef26d0c33
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-05-24 13:53:59 +0530

    ✅ Add solution for: Java program to reverse the digit.

---
+++ exercises/JavaProgramToReverseTheDigit.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+public class Solution { 
+    public static void main(String[] args) { 
+    Scanner scanner = new Scanner(System.in);
+    int number = scanner.nextInt();
+    int reversed = 0;
+    while (number != 0){
+        int digit = number % 10;
+        reversed = reversed * 10 + digit;
+        number = number/10;
+    }
+    System.out.println(reversed);
+    scanner.close(); 
+   } 
+} 
