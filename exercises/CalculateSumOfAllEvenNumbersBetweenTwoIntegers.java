459356a2ac7cee7bed077c47de96b383dd2cbc67
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-05-24 12:57:27 +0530

    ✅ Add solution for: Calculate sum of all even numbers between two integers.

---
+++ exercises/CalculateSumOfAllEvenNumbersBetweenTwoIntegers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+        int sum = 0;
+        for (int i = start; i <= end; i++) {
+           if (i % 2 == 0) {
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
